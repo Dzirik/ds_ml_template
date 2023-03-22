085ad1c913c854a85985e72bfa7f3535cff8654f
Dzirik <jiri.palek@gmail.com>
2023-03-22 09:35:32 +0100

    Update win install script

--- install.ps1
+++ install.ps1
@@ -1,7 +1,10 @@
-# policy: https://www.easy365manager.com/script-ps1-cannot-be-loaded-because-running-scripts-is-disabled-on-this-system/
-# - Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser.
+#Setting execution policy:
+# https://www.easy365manager.com/script-ps1-cannot-be-loaded-because-running-scripts-is-disabled-on-this-system/
+# Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser.
 
 Copy "make_config_template.mk" "make_config.mk"
 Copy ".\configurations\python_repo.conf" ".\configurations\python_personal.conf"
 Copy ".\notebooks\template\template_notebook_final.py" ".\notebooks\raw\playground_notebook.py"
-make create-venv
+
+# error in powershell
+# make create-venv
