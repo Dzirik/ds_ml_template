84cdf3bd6df0823eb8d61219df5e6d2300eda9ff
Dzirik <jiri.palek@gmail.com>
2023-03-22 09:11:49 +0100

    Add files copy

---
+++ install.ps1
@@ -0,0 +1,5 @@
+# policy: https://www.easy365manager.com/script-ps1-cannot-be-loaded-because-running-scripts-is-disabled-on-this-system/
+# - Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser.
+
+Copy "make_config_template.mk" "make_config.mk"
+Copy ".\configurations\python_repo.conf" ".\configurations\python_personal.conf"
