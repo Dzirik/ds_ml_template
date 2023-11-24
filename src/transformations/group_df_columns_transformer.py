36d39f367bb8136a50dd1c57f088ae974546eda6
Dzirik <jiri.palek@gmail.com>
2023-11-24 09:11:34 +0100

    Update transformers

--- src/transformations/group_df_columns_transformer.py
+++ src/transformations/group_df_columns_transformer.py
@@ -7,7 +7,7 @@ Notes:
     - df.applymap(SomeFunction)
     - df[list_of_columns].apply(SomeFunction)
 """
-from typing import List, Any
+from typing import List, Any, Dict
 
 from pandas import DataFrame, Series
 
@@ -31,14 +31,17 @@ class GroupDFColumnsTransformer(BaseTransformer):  # type:ignore
         self._series_out: Series
 
     # pylint: disable=arguments-differ
-    def fit(self, df: DataFrame, attrs: List[str], grouping_fun: str = "mean", pom: Any = 0) -> None:
+    # pylint: disable=too-many-branches
+    def fit(self, df: DataFrame, attrs: List[str], grouping_fun: str, params: Dict[str, Any]) -> None:
         """
         Fits.
+        - "shift_*": params = {"shift": int}.
+        - "div": params = {"zero_div_replacement": float}
         :param df: DataFrame. Dataframe to be grouped.
         :param attrs: List[str]. List of parameters to be grouped. Datetime has to be included as first one.
         :param grouping_fun: str. Function to be performed. Currently available "mean", "std", "sum", "empty"
-                                  "shift_future", "shift_past", "minus".
-        :param pom. Any. Additional parameter if needed.
+                                  "shift_future", "shift_past", "minus", "div".
+        :param params. Dict[str, Any]. Additional parameters if needed.
         """
         if grouping_fun == "mean":
             self._series_out = df[attrs].mean(axis=1)
@@ -49,9 +52,11 @@ class GroupDFColumnsTransformer(BaseTransformer):  # type:ignore
         elif grouping_fun == "empty":
             self._series_out = Series(data=[""] * df.shape[0], index=df.index)
         elif grouping_fun == "shift_future":
+            pom = params["shift"]
             self._series_out = df[attrs[0]].tolist()
             self._series_out = Series(self._series_out[pom:] + [self._series_out[-1]] * pom, index=df.index)
         elif grouping_fun == "shift_past":
+            pom = params["shift"]
             self._series_out = df[attrs[0]].tolist()
             self._series_out = Series([self._series_out[0]] * pom + self._series_out[:-pom], index=df.index)
         elif grouping_fun == "minus":
@@ -59,20 +64,31 @@ class GroupDFColumnsTransformer(BaseTransformer):  # type:ignore
                 self._series_out = - df[attrs[0]]
             else:
                 self._series_out = None
+        elif grouping_fun == "div":
+            if len(attrs) == 2:
+                pom = params["zero_div_replacement"]
+                numerator = list(df[attrs[0]])
+                denominator = list(df[attrs[1]])
+                result = [x / y if y != 0 else pom for x, y in zip(numerator, denominator)]
+                self._series_out = Series(result, index=df.index)
+            else:
+                self._series_out = None
         else:
             ExceptionExecutioner(NoProperOptionInIf).log_and_raise(description=self._class_info.class_type + " " +
                                                                                self._class_info.class_name)
 
-    def fit_predict(self, df: DataFrame, attrs: List[str], grouping_fun: str = "mean", pom: Any = 0) -> Series:
+    # pylint: enable=too-many-branches
+
+    def fit_predict(self, df: DataFrame, attrs: List[str], grouping_fun: str, params: Dict[str, Any]) -> Series:
         """
         Fits and predicts.
         :param df: DataFrame. Dataframe to be grouped.
         :param attrs: List[str]. List of parameters to be grouped. Datetime has to be included as first one.
         :param grouping_fun: str. Function to be performed. Currently available.
-        :param pom. Any. Additional parameter if needed.
+        :param params. Dict[str, Any]. Additional parameters if needed.
         :return: Series. Grouped data series.
         """
-        self.fit(df, attrs, grouping_fun, pom)
+        self.fit(df, attrs, grouping_fun, params)
         return self._series_out
 
     def predict(self) -> Series:
