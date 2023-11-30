baf4a220e9ac25aa58fc14367f3f97cbb79b3de0
Dzirik <jiri.palek@gmail.com>
2023-11-30 09:57:31 +0100

    Update saver and loader

--- src/data/saver_and_loader.py
+++ src/data/saver_and_loader.py
@@ -3,12 +3,17 @@ Saver and loader
 
 Class for simplification of saving and loading.
 """
+import json
 import os
 import pickle
 from typing import Any
 
+import typedload
 from pandas import DataFrame, read_pickle, read_csv
+from pyhocon import ConfigFactory
+from tensorflow import keras
 
+from src.exceptions.data_exception import FileNotFound
 from src.utils.config import Config
 
 
@@ -31,8 +36,9 @@ class SaverAndLoader():
     """
 
     def __init__(self) -> None:
-        self._decimal = ","
-        self._sep = ";"
+        self._decimal = "."
+        self._sep = ","
+        self._csv_date_time_format = "%Y/%m/%d %H:%M:%S"
 
     def set_csv_params(self, decimal: str, sep: str) -> None:
         """
@@ -43,6 +49,20 @@ class SaverAndLoader():
         self._decimal = decimal
         self._sep = sep
 
+    @staticmethod
+    def load_keras_model(file_name: str, where: str = "models") -> Any:
+        """
+        Loads a keras model.
+        :param file_name: str. Name of the file without .keras suffix.
+        :param where: str. Name of the path from the config file.
+        :return: Any. Keras model.
+        """
+        path = get_path(file_name, where, ".keras")
+        if os.path.exists(path):
+            return keras.models.load_model(path)
+        else:
+            raise FileNotFound(description=f"File {path} was not found on selected path.")
+
     def save_dataframe_to_csv(self, df: DataFrame, file_name: str, where: str = "raw_data") -> None:
         """
         Saves a DataFrame to a csv file.
@@ -51,7 +71,7 @@ class SaverAndLoader():
         :param where: str. Name of the path from the config file.
         """
         path = get_path(file_name, where, ".csv")
-        df.to_csv(path, index=False, decimal=self._decimal, sep=self._sep)
+        df.to_csv(path, index=False, decimal=self._decimal, sep=self._sep, date_format=self._csv_date_time_format)
 
     def load_dataframe_from_csv(self, file_name: str, where: str = "raw_data", min_size: int = 2) -> DataFrame:
         """
@@ -63,9 +83,11 @@ class SaverAndLoader():
         """
         path = get_path(file_name, where, ".csv")
         if os.path.exists(path):
-            # zero len doesnt work with csv files in cry project, it had length 2.
+            # zero len doesn't work with csv files in some cases when small
             if os.path.getsize(path) > min_size:
                 return read_csv(path, decimal=self._decimal, sep=self._sep)
+        else:
+            raise FileNotFound(description=f"File {path} was not found on selected path.")
         return DataFrame()
 
     @staticmethod
@@ -93,6 +115,8 @@ class SaverAndLoader():
             if os.path.getsize(path) > min_size:
                 df = read_pickle(path)
                 return df
+        else:
+            raise FileNotFound(description=f"File {path} was not found on selected path.")
         return DataFrame()
 
     @staticmethod
@@ -123,6 +147,8 @@ class SaverAndLoader():
                 with open(path, 'rb') as handle:
                     loaded = pickle.load(handle)
                 return loaded
+        else:
+            raise FileNotFound(description=f"File {path} was not found on selected path.")
         return None
 
     @staticmethod
@@ -135,6 +161,33 @@ class SaverAndLoader():
         path = get_path(file_name, where)
         if os.path.exists(path):
             os.remove(path)
+
+    @staticmethod
+    def save_config_data(config_data: Any, file_name: str, where: str = "raw_data") -> None:
+        """
+        Converts the config data to a dictionary and saves it as .conf file.
+        :param config_data: Any. Configuration's named tuple (see FEConfig, MMTradingConfig, ...)
+        :param file_name: str. File name without .pkl.
+        :param where: str. Name of the path from the config file.
+        """
+        path = get_path(file_name, where, ".conf")
+        data = typedload.dump(config_data)
+        with open(path, "w", encoding="utf8") as outfile:
+            json.dump(data, outfile)
+
+    @staticmethod
+    def load_config_data(file_name: str, config_data_structure: Any, where: str = "raw_data") -> Any:
+        """
+        Loads the config data.
+        :param file_name: str. File name without .pkl.
+        :param config_data_structure: Any. Named tuple of the config.
+        :param where: str. Name of the path from the config file.
+        """
+        path = get_path(file_name, where, ".conf")
+        if os.path.exists(path):
+            return typedload.load(ConfigFactory.parse_file(path), config_data_structure)
+        else:
+            raise FileNotFound(description=f"File {path} was not found on selected path.")
 
 
 if __name__ == "__main__":
