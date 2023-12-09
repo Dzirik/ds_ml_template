e82167622340765fd8a8ae5d4bf17a33424db85c
Dzirik <jiri.palek@gmail.com>
2023-12-09 09:27:48 +0100

    Update global constants

--- src/constants/global_constants.py
+++ src/constants/global_constants.py
@@ -18,7 +18,7 @@ DEFAULT_RUNNING_UNIT_TESTS = "False"
 # When a bad logger config file is put into logger reading, then a special logger
 # is created and the message is added to the following file.
 # The file name should be the same as in config files for logger!
-SPECIAL_LOGGER_FILE_NAME = "../../reports/python_log.log"
+SPECIAL_LOGGER_FILE_NAME = "../../logs/logger_file_limit_console.log"
 
 # Folders widely used in repository
 
